d1a09ce01bf564a9e0f537796fe390304bdaa574
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-07-12 16:14:53 +0530

    fdbfdhfehfh

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,16 @@
 export const CATEGORIES = ["Men", "Women", "Kids", "Accessories", "Other"];
 
 export const ROLES = ["user", "admin"];
+
+export const STOCK_IMAGES = [
+	"https://i.pinimg.com/60x60/7d/3f/2f/7d3f2f6cf9664994ee3ae56a268f26b9.jpg",
+	"https://i.pinimg.com/60x60/04/5a/cb/045acbc218006ec6857a31ebf48330e9.jpg",
+	"https://i.pinimg.com/60x60/81/35/c5/8135c5f8ef39526076ac30432ad403b3.jpg",
+	"https://i.pinimg.com/60x60/94/8f/7a/948f7a63203b896e38bbc94584595185.jpg",
+	"https://i.pinimg.com/60x60/30/c2/f8/30c2f8ba91a415a87894fd3a2b98cc16.jpg",
+	"https://i.pinimg.com/60x60/57/de/cf/57decfab8feb4ae820f423ca854a9c40.jpg",
+	"https://i.pinimg.com/60x60/50/db/cd/50dbcd4265648f000435fe6916fc1f64.jpg",
+	"https://i.pinimg.com/60x60/3c/46/58/3c4658c9e526fa01ca2502a1bd0666b7.jpg",
+	"https://i.pinimg.com/60x60/db/08/95/db0895011eaf57e30c961061bc8daa0f.jpg",
+	"https://i.pinimg.com/60x60/c2/4b/18/c24b18486862ad0cb712d490c5023f5f.jpg",
+];
