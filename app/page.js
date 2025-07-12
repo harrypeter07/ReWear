ff18ddabdc1869067ff3f43aa1d273a0bf2d391f
ritikaj22 <jainr_1@rknec.edu>
2025-07-12 12:37:24 +0530

    Image url

--- app/page.js
+++ app/page.js
@@ -11,19 +11,19 @@ export default function Home() {
       id: 1,
       title: "Vintage Denim Jacket",
       category: "Jackets",
-      image: "/images/denim-jacket.jpg"
+      image: "https://www.alamodelabel.in/cdn/shop/files/AD15F82B-CAD5-4B7A-9B2E-814B092A3068_600x600_crop_center.jpg?v=1749207515"
     },
     {
       id: 2,
       title: "Summer Floral Dress",
       category: "Dresses",
-      image: "/images/floral-dress.jpg"
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtKdDLXw5t-0a7qxJB8p7e16JPkeuU3N-CEg&s"
     },
     {
       id: 3,
       title: "Classic White Sneakers",
       category: "Shoes",
-      image: "/images/sneakers.jpg"
+      image: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/24908578/2023/9/12/da8743b9-d2aa-4d7e-a4d4-c63d1ea9e9ad1694490216796ASIANBoysWhitePerforationsSneakers1.jpg"
     }
   ];
 
